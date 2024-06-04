8c3f3ced322fcadce5fbe0598825479824108796
freyamade <freya.broderick@honu.ai>
2024-06-04 10:31:14 +0200

    moved gearslots to ingamechar data since it's only needed there

--- SavageAim/Data/SACharacter.cs
+++ SavageAim/Data/SACharacter.cs
@@ -2,11 +2,6 @@ using System.Text.Json.Serialization;
 
 namespace SavageAimPlugin.Data;
 
-public record class BISSummary (
-    [property: JsonPropertyName("name")] string Name,
-    [property: JsonPropertyName("id")] uint ID
-);
-
 public record class SACharacter(
     [property: JsonPropertyName("bis_lists")] BISSummary[] BISSummaries,
     [property: JsonPropertyName("id")] uint ID,
